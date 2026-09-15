2035540a99145170addf5388b90361d5872b17fb
Strawreo <strawreomilk@gmail.com>
2026-09-15 18:20:06 -0300

    /feat Cursor to battle grid

---
+++ Scripts/cursor.gd
@@ -0,0 +1,18 @@
+extends Node2D
+var grid_pos:Vector2i = Vector2i(1,1)
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Right") and grid_pos.x != 8:
+		position.x += 20
+		grid_pos.x += 1
+	if Input.is_action_just_pressed("Left") and grid_pos.x != 1:
+		position.x -= 20
+		grid_pos.x -= 1
+	if Input.is_action_just_pressed("Down") and grid_pos.y != 8:
+		position.y += 16
+		grid_pos.y += 1
+	if Input.is_action_just_pressed("Up") and grid_pos.y != 1:
+		position.y -= 16
+		grid_pos.y -= 1
