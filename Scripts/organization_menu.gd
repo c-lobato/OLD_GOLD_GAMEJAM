25f246daa4ea8ee9c4c7dc1d2d9d61a4b0192ded
c-lobato <caiolpbarreto@gmail.com>
2026-09-16 17:17:03 -0300

    organização da cena de party e criação do cursor

--- Scripts/organization_menu.gd
+++ Scripts/organization_menu.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
+var org_node
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready(): 
 	$Control/Battle.grab_focus()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,3 +12,7 @@ func _process(delta: float) -> void:
 
 func _on_battle_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Battle.tscn")
+
+
+func _on_tripulante_1_pressed() -> void:
+	pass # Replace with function body.
