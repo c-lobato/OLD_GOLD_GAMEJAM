d80e0bb9a6e8dce98866757a98cfe7d484b77094
Strawreo <strawreomilk@gmail.com>
2026-09-16 13:51:40 -0300

    feat: fix

--- Scripts/organization_menu.gd
+++ Scripts/organization_menu.gd
@@ -10,10 +10,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/SelectionMenu.tscn")
-
-
 func _on_battle_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Battle.tscn")
