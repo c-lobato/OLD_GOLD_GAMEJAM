3f7e5be1756a15b8c9be346e4bd63ed2ba358203
Strawreo <strawreomilk@gmail.com>
2026-09-16 19:26:31 -0300

    feat: assets + coord bug fix

--- Scripts/agent.gd
+++ Scripts/agent.gd
@@ -1,33 +1,22 @@
+class_name Agent
 extends Node2D
 
-var grid_pos: Vector2i = Vector2i(0,0)
-var nome: String = ""
-var hp_max: int = 10
-var hp_atual: int = 10
-var speed: int = 1
-var damage: int = 3
-var tipo: String = ""
-var cena_agente: PackedScene
-var itens_equipados: Array[Resource] = []
+var dados: AgenteData
 
 func carregar_dados(data: AgenteData) -> void:
 	if data == null:
-		push_error("Nenhum dado de agente foi fornecido!")
+		push_error("Nenhum dado fornecido!")
 		return
-		
-	grid_pos = data.grid_pos
-	nome = data.nome
-	hp_max = data.hp_max
-	hp_atual = data.hp_atual
-	speed = data.speed
-	damage = data.damage
-	tipo = data.tipo
-	cena_agente = data.cena_agente
-	itens_equipados = data.itens_equipados
+	
+	dados = data
+	$AnimatedSprite2D.animation = dados.tipo 
+	$AnimatedSprite2D.play()
+	
+	print(dados.nome, dados.grid_pos)
 
-	position.x = grid_pos.x * 16 
-	position.y == grid_pos.y * 16
 
+func atacar() -> void:
+	print("Agente realizando ataque genérico!")
 	
 
 	
